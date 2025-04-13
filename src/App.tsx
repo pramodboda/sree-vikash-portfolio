51923d8c859ef48324e720b5c76e0ef3e0df4044
Pramod Boda <PB493939@Haleon.ad>
2025-04-13 17:20:06 +0530

    Circular Gallery

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,16 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider as MuiThemeProvider } from "./contexts/theme/ThemeContext";
 
 import Hero_Section from "./components/hero/Hero_Section";
+import CircularGallery from "./components/circular-gallery/CircularGallery";
+
 function App() {
   return (
     <MuiThemeProvider>
       <CssBaseline />
       <Hero_Section />
+      <div style={{ height: "600px", position: "relative" }}>
+        <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
+      </div>
     </MuiThemeProvider>
   );
 }
