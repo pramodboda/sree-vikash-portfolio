44e392ca1e03694490d8f055a9495e0e09627941
pramodboda <pramodkumarboda@gmail.com>
2025-04-16 21:32:29 +0530

    UPDATED: Services_Section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider as MuiThemeProvider } from "./contexts/theme/ThemeContext
 
 import Hero_Section from "./components/hero/Hero_Section";
 import CircularGallery from "./components/circular-gallery/CircularGallery";
+import Services_Section from "./components/services/Servies_Section";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <div style={{ height: "600px", position: "relative" }}>
         <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
       </div>
+      <Services_Section/>
     </MuiThemeProvider>
   );
 }
--- src/components/services/Servies_Section.tsx
+++ src/components/services/Servies_Section.tsx
@@ -5,13 +5,13 @@ const demoItems = [
     { link: '#', text: 'Mojave', image: 'https://picsum.photos/600/400?random=1' },
     { link: '#', text: 'Sonoma', image: 'https://picsum.photos/600/400?random=2' },
     { link: '#', text: 'Monterey', image: 'https://picsum.photos/600/400?random=3' },
-    { link: '#', text: 'Sequoia', image: 'https://picsum.photos/600/400?random=4' }
+    { link: '#', text: 'Sequoia', image: 'https://picsum.photos/600/400?random=4' }]
 
-const ServicesSection: React.FC = () => {
+const Services_Section: React.FC = () => {
     return (<>
         <div style={{ height: '600px', position: 'relative' }}>
             <FlowingMenu items={demoItems} />
         </div>
     </>)
 }
-export default ServicesSection;
+export default Services_Section;
