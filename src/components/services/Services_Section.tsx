c35b9d679df009897192b7d68f07d2bca3b3e8f6
pramodboda <pramodkumarboda@gmail.com>
2025-06-05 05:18:15 +0530

    UPDATE: "Animations"

--- src/components/services/Services_Section.tsx
+++ src/components/services/Services_Section.tsx
@@ -6,18 +6,24 @@ const demoItems = [
   {
     link: "#",
     text: "Portrait Photography",
-    image:
-      "https://www.sreevikash.com/wp-content/uploads/2021/01/Student-Referral6.jpg",
+    // image: "https://www.sreevikash.com/wp-content/uploads/2021/01/Student-Referral6.jpg",
+
+    // image: "https://www.sreevikash.com/wp-content/uploads/2022/10/SVM01452-scaled.jpg",
+    image:"https://www.sreevikash.com/wp-content/uploads/2021/01/corporate-culture-photos-bangalore_sreevikash-2.jpg"
+
+    
   },
   {
     link: "#",
     text: "Wedding Photography & Film",
-    image: "https://picsum.photos/600/400?random=2",
+    // image: "https://picsum.photos/600/400?random=2",
+    image:"https://www.sreevikash.com/wp-content/uploads/2025/01/sree_vikash_photography_wedding_pre_wedding_bangalore_best.jpg"
   },
   {
     link: "#",
     text: "Corporate Photography & Film",
-    image: "https://picsum.photos/600/400?random=3",
+    // image: "https://picsum.photos/600/400?random=3",
+    image:"https://www.sreevikash.com/wp-content/uploads/2023/09/corporate_lifestyle_photographers_bangalore_sreevikash_29.jpg"
   },
   {
     link: "#",
